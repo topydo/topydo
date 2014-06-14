21a4cd9e059357988e1d67cd1e79ac01c4c540a5
Bram Schoenmakers <me@bramschoenmakers.nl>
2014-06-14 15:04:41 +0200

    Add more flags to Config.py.

--- Config.py
+++ Config.py
@@ -2,5 +2,11 @@
 This module contains some definitions to configure the application.
 """
 
+DEFAULT_ACTION = 'ls'
+
+FILENAME = 'todo.txt'
+COMPLETED_FILENAME = 'done.txt'
+
 TAG_START = 't'
 TAG_DUE = 'due'
+TAG_STAR = 'star'
