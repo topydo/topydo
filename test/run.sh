823fe7a1d61b5ba3cd403736f0aca0a7fe701257
Bram Schoenmakers <me@bramschoenmakers.nl>
2014-06-14 15:20:24 +0200

    Convenience script to run all tests.

---
+++ test/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PYTHONPATH=..
+
+if [ -n "$1" ]; then
+  TESTS=$1
+else
+  TESTS="SorterTest TodoBaseTest TodoFileTest TodoTest TodoListTest"
+fi
+
+for TEST in $TESTS; do
+  python -m unittest "$TEST"
+done
