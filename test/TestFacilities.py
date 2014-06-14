b688f74445416ff854eb1a70b59eb4801d748738
Bram Schoenmakers <me@bramschoenmakers.nl>
2014-06-14 15:04:08 +0200

    Add another convenience method to easily compare lists.

--- test/TestFacilities.py
+++ test/TestFacilities.py
@@ -8,3 +8,6 @@ def load_file(p_filename):
     todofile = TodoFile.TodoFile(p_filename)
     return [Todo.Todo(src) for src in todofile.read()]
 
+def todolist_to_string(p_list):
+    """ Converts a todo list to a single string. """
+    return '\n'.join([str(t) for t in p_list])
