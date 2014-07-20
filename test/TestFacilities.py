4dbcc84db67ef6355e40139e0e604a848dbb1704
Bram Schoenmakers <me@bramschoenmakers.nl>
2014-07-20 22:06:59 +0200

    Add function to return a list of raw todo strings.

--- test/TestFacilities.py
+++ test/TestFacilities.py
@@ -5,8 +5,16 @@ def load_file(p_filename):
     """
     Loads a todo file from the given filename and returns a list of todos.
     """
+    todolist = load_file_to_raw_list(p_filename)
+    return [Todo.Todo(src, number + 1) for number, src in enumerate(todolist)]
+
+def load_file_to_raw_list(p_filename):
+    """
+    Loads a todo file from the given filename and returns a list of todo
+    strings (unparsed).
+    """
     todofile = TodoFile.TodoFile(p_filename)
-    return [Todo.Todo(src) for src in todofile.read()]
+    return todofile.read()
 
 def todolist_to_string(p_list):
     """ Converts a todo list to a single string. """
