b8cd5ae1286cf8092f5e0af5e34fae1bc5b962a6
Bram Schoenmakers <me@bramschoenmakers.nl>
2014-11-02 18:06:06 +0100

    Fix test case names.

--- test/TodoListTest.py
+++ test/TodoListTest.py
@@ -1,16 +1,16 @@
 # Topydo - A todo.txt client written in Python.
 # Copyright (C) 2014 Bram Schoenmakers <me@bramschoenmakers.nl>
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -169,13 +169,13 @@ class TodoListTester(unittest.TestCase):
         self.assertEquals(self.todolist.todo(1).priority(), 'F')
         self.assertTrue(self.todolist.is_dirty())
 
-    def todo_todo_priority2(self):
+    def test_todo_priority2(self):
         todo = self.todolist.todo(1)
         self.todolist.set_priority(todo, 'C')
 
         self.assertFalse(self.todolist.is_dirty())
 
-    def todo_erase(self):
+    def test_erase(self):
         self.todolist.erase()
 
         self.assertEquals(self.todolist.count(), 0)
