c5b9ede5cb91583af294603ab720c840aecedc2c
Bram Schoenmakers <me@bramschoenmakers.nl>
2014-11-08 20:47:53 +0100

    Add classifiers and a long description to setup.py.

--- setup.py
+++ setup.py
@@ -10,4 +10,23 @@ setup(
     url = "https://github.com/bram85/topydo",
     download_url = "https://github.com/bram85/topydo/archive/master.zip",
     scripts = ["bin/topydo"],
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2",
+        "Topic :: Utilities",
+    ],
+    long_description = """\
+Topydo is a todo list application using the todo.txt format. It is heavily inspired by the todo.txt CLI by Gina Trapani. This tool is actually a merge between the todo.txt CLI and a number of extensions that I wrote on top of the CLI. These extensions are:
+
+* Set due and start dates;
+* Custom sorting;
+* Dealing with tags;
+* Maintain dependencies between todo items;
+* Allow todos to recur;
+* Some conveniences when adding new items (e.g. adding creation date and use relative dates)
+"""
 )
