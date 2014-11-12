bdc3a41c255d2b0ab9407c048ac679f8aa42f317
Bram Schoenmakers <me@bramschoenmakers.nl>
2014-11-12 20:03:38 +0100

    Return False when any of the operands is invalid.

--- topydo/lib/Filter.py
+++ topydo/lib/Filter.py
@@ -168,6 +168,9 @@ class OrdinalTagFilter(Filter):
 
             if not operand2:
                 operand2 = date_string_to_date(self.value)
+
+            if not operand1 or not operand2:
+                return False
         else:
             try:
                 operand1 = int(p_todo.tag_value(self.key))
