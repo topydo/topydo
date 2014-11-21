eb6bb6f2e411fc4424fb63f53124654efd10733a
Bram Schoenmakers <me@bramschoenmakers.nl>
2014-11-21 23:49:43 +0100

    Add edit subcommand.

---
+++ topydo/lib/EditCommand.py
@@ -0,0 +1,31 @@
+# Topydo - A todo.txt client written in Python.
+# Copyright (C) 2014 Bram Schoenmakers <me@bramschoenmakers.nl>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+from subprocess import call
+
+from topydo.lib.Command import Command
+from topydo.lib.Config import config
+
+class EditCommand(Command):
+    def __init__(self, p_todolist, *p_args):
+        super(EditCommand, self).__init__([], None)
+
+    def execute(self):
+        editor = os.environ['EDITOR'] or 'vi'
+        todo = config().todotxt()
+
+        return call([editor, todo]) == 0
