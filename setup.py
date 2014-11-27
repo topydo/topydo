03e7eb881c1659fd4b0a21edb309c32528ff6e15
Bram Schoenmakers <me@bramschoenmakers.nl>
2014-11-27 21:39:52 +0100

    Change download URL.

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name = "Topydo",
+    name = "topydo",
     packages = ["topydo", "topydo.lib", "topydo.cli"],
     version = "0.1",
     description = "A todo list application using the todo.txt format.",
     author = "Bram Schoenmakers",
     author_email = "me@bramschoenmakers.nl",
     url = "https://github.com/bram85/topydo",
-    download_url = "https://github.com/bram85/topydo/archive/master.zip",
+    download_url = "https://github.com/bram85/topydo/archive/stable.zip",
     scripts = ["bin/topydo"],
     classifiers = [
         "Development Status :: 4 - Beta",
@@ -20,7 +20,7 @@ setup(
         "Topic :: Utilities",
     ],
     long_description = """\
-Topydo is a todo list application using the todo.txt format. It is heavily inspired by the todo.txt CLI by Gina Trapani. This tool is actually a merge between the todo.txt CLI and a number of extensions that I wrote on top of the CLI. These extensions are:
+topydo is a todo list application using the todo.txt format. It is heavily inspired by the todo.txt CLI by Gina Trapani. This tool is actually a merge between the todo.txt CLI and a number of extensions that I wrote on top of the CLI. These extensions are:
 
 * Set due and start dates;
 * Custom sorting;
