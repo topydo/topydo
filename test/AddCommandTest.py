9426a78966275f80c315831e455a6ddbfb88fd9c
Bram Schoenmakers <me@bramschoenmakers.nl>
2015-02-04 17:30:05 +0100

    Import unittest module where necessary.

--- test/AddCommandTest.py
+++ test/AddCommandTest.py
@@ -15,6 +15,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from datetime import date
+import unittest
 
 from topydo.lib import AddCommand
 from topydo.lib import ListCommand
--- test/DoCommandTest.py
+++ test/DoCommandTest.py
@@ -15,6 +15,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from datetime import date, timedelta
+import unittest
 
 import CommandTest
 from topydo.lib.DoCommand import DoCommand
