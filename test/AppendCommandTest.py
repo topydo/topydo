239855249a8086caefd95e70b2a279fc4595b2dc
Bram Schoenmakers <me@bramschoenmakers.nl>
2015-02-04 17:30:05 +0100

    Import unittest module where necessary.

--- test/AppendCommandTest.py
+++ test/AppendCommandTest.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import unittest
+
 from topydo.lib.AppendCommand import AppendCommand
 import CommandTest
 from topydo.lib.TodoList import TodoList
--- test/SortCommandTest.py
+++ test/SortCommandTest.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import unittest
+
 import CommandTest
 from topydo.lib.Config import config
 from topydo.lib.SortCommand import SortCommand
