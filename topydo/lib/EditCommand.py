8542cffd981fa606a3e38e04c02f52394d8113cd
Bram Schoenmakers <me@bramschoenmakers.nl>
2015-05-15 20:55:05 +0200

    Minor pylint and style adjustment.

--- topydo/lib/EditCommand.py
+++ topydo/lib/EditCommand.py
@@ -55,9 +55,9 @@ class EditCommand(MultiCommand, ListCommand):
 
         return f
 
-    def _todos_from_temp(self, temp_file):
-        temp_file.seek(0)
-        todos = temp_file.read().splitlines()
+    def _todos_from_temp(self, p_temp_file):
+        p_temp_file.seek(0)
+        todos = p_temp_file.read().splitlines()
 
         todo_objs = []
         for todo in todos:
@@ -65,10 +65,10 @@ class EditCommand(MultiCommand, ListCommand):
 
         return todo_objs
 
-    def _open_in_editor(self, temp_file, editor):
+    def _open_in_editor(self, p_temp_file, p_editor):
         try:
-            return check_call([editor, temp_file.name])
-        except(CalledProcessError):
+            return check_call([p_editor, p_temp_file.name])
+        except CalledProcessError:
             self.error('Something went wrong in the editor...')
             return 1
 
