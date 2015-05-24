6ab221a5a5cd37a8c48468ec383b3e18cabbe7fd
Jacek Sowiński <mruwek@gmail.com>
2015-05-24 13:49:48 +0200

    Add new ExpressionCommand class
    Currently it contains only `_view` and `_filters` methods, which were
    previously defined in ListCommand. Now both EditCommand and ListCommand
    will inherit from it to solve problems inside EditCommand.
    
    This fixes #28

--- topydo/commands/EditCommand.py
+++ topydo/commands/EditCommand.py
@@ -20,7 +20,7 @@ import tempfile
 
 from six import text_type, u
 
-from topydo.commands.ListCommand import ListCommand
+from topydo.lib.ExpressionCommand import ExpressionCommand
 from topydo.lib.MultiCommand import MultiCommand
 from topydo.lib.Config import config
 from topydo.lib.Todo import Todo
@@ -32,7 +32,7 @@ from topydo.lib.PrettyPrinterFilter import PrettyPrinterNumbers
 # cannot use super() inside the class itself
 BASE_TODOLIST = lambda tl: super(TodoList, tl)
 
-class EditCommand(MultiCommand, ListCommand):
+class EditCommand(MultiCommand, ExpressionCommand):
     def __init__(self, p_args, p_todolist, p_output, p_error, p_input):
         super(EditCommand, self).__init__(p_args, p_todolist, p_output,
             p_error, p_input)
