6f33283fddccf27aa0f9b03797570b1c7800bbf2
Bram Schoenmakers <me@bramschoenmakers.nl>
2015-05-27 17:13:12 +0200

    Version bump.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = "topydo",
     packages = ["topydo", "topydo.lib", "topydo.cli"],
-    version = "0.3.1",
+    version = "0.3.2",
     description = "A command-line todo list application using the todo.txt format.",
     author = "Bram Schoenmakers",
     author_email = "me@bramschoenmakers.nl",
