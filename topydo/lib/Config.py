7120f86676a9cf1bc3a427d9448fef8367b5f474
Jacek Sowiński <mruwek@gmail.com>
2015-10-30 23:14:50 +0100

    Fix tests and config for list_format

--- topydo/lib/Config.py
+++ topydo/lib/Config.py
@@ -69,7 +69,7 @@ class _Config:
                 'hide_tags': 'id,p,ical',
                 'indent': '0',
                 'list_limit': '-1',
-                'list_format': '|%I| %c %{(}p{)} %s',
+                'list_format': '|%I| %x %c %{(}p{)} %s %k',
             },
 
             'tags': {
