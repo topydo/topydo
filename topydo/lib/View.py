3bb55c37cb63b2d7e74991c2a1caf771333d0db2
Bram Schoenmakers <bram@topydo.org>
2016-11-27 14:24:29 +0100

    Update docstring

--- topydo/lib/View.py
+++ topydo/lib/View.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-""" A view is a list of todos, sorted and filtered. """
+""" A view is a list of todos, sorted, grouped and filtered. """
 
 
 class View(object):
