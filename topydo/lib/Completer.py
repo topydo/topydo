3f9d6367ec49f033ac85c4633bca5fc7127582be
Jacek Sowiński <mruwek@gmail.com>
2017-02-12 23:26:58 +0100

    Reuse CompleterBase code in prompt completer
    Also introduce completion of due: dates in column completer.

--- topydo/lib/Completer.py
+++ topydo/lib/Completer.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import datetime
+
 from topydo.Commands import _SUBCOMMAND_MAP
 from topydo.lib.Config import config
 
@@ -25,34 +27,77 @@ def _get_subcmds():
     return sorted(subcmd_map.keys())
 
 
+def date_suggestions():
+    """
+    Returns a list of relative date that is presented to the user as auto
+    complete suggestions.
+    """
+    # don't use strftime, prevent locales to kick in
+    days_of_week = {
+        0: "Monday",
+        1: "Tuesday",
+        2: "Wednesday",
+        3: "Thursday",
+        4: "Friday",
+        5: "Saturday",
+        6: "Sunday"
+    }
+
+    dates = [
+        'today',
+        'tomorrow',
+    ]
+
+    # show days of week up to next week
+    dow = datetime.date.today().weekday()
+    for i in range(dow + 2 % 7, dow + 7):
+        dates.append(days_of_week[i % 7])
+
+    # and some more relative days starting from next week
+    dates += ["1w", "2w", "1m", "2m", "3m", "1y"]
+
+    return dates
+
+
 class CompleterBase(object):
     def __init__(self, p_todolist):
         self.todolist = p_todolist
         self._subcmds = _get_subcmds()
 
-    def _complete_context(self, p_word):
+    def contexts(self, p_word):
         completions = ['@' + context for context in self.todolist.contexts() if
                        context.startswith(p_word[1:])]
         return completions
 
-    def _complete_project(self, p_word):
+    def projects(self, p_word):
         completions = ['+' + project for project in self.todolist.projects() if
                        project.startswith(p_word[1:])]
         return completions
 
-    def _complete_subcmd(self, p_word):
+    def subcmds(self, p_word):
         completions = [cmd for cmd in self._subcmds if
                        cmd.startswith(p_word)]
         return completions
+
+    def dates(self, p_word):
+        dates = []
+        for date in date_suggestions():
+            due_date = 'due:' + date
+            if due_date.startswith(p_word):
+                dates.append(due_date)
+
+        return dates
 
     def get_completions(self, p_word, p_is_first_word=False):
         completions = []
 
         if p_word.startswith('+'):
-            completions = self._complete_project(p_word)
+            completions = self.projects(p_word)
         elif p_word.startswith('@'):
-            completions = self._complete_context(p_word)
+            completions = self.contexts(p_word)
+        elif p_word.startswith('due:'):
+            completions = self.dates(p_word)
         elif p_is_first_word:
-            completions = self._complete_subcmd(p_word)
+            completions = self.subcmds(p_word)
 
         return completions
--- topydo/ui/prompt/TopydoCompleter.py
+++ topydo/ui/prompt/TopydoCompleter.py
@@ -19,64 +19,22 @@ This module provides a completer class that can be used by the prompt provided
 by the prompt toolkit.
 """
 
-import datetime
 import re
 
 from prompt_toolkit.completion import Completer, Completion
-from topydo.Commands import _SUBCOMMAND_MAP
+from topydo.lib.Completer import CompleterBase, date_suggestions
 from topydo.lib.Config import config
 from topydo.lib.RelativeDate import relative_date_to_date
 
 
-def _subcommands(p_word_before_cursor):
-    """ Generator for subcommand name completion. """
-    sc_map = config().aliases()
-    sc_map.update(_SUBCOMMAND_MAP)
-    subcommands = [sc for sc in sorted(sc_map.keys()) if
-                   sc.startswith(p_word_before_cursor)]
-    for command in subcommands:
-        yield Completion(command, -len(p_word_before_cursor))
-
-
 def _dates(p_word_before_cursor):
     """ Generator for date completion. """
-    def _date_suggestions():
-        """
-        Returns a list of relative date that is presented to the user as auto
-        complete suggestions.
-        """
-        # don't use strftime, prevent locales to kick in
-        days_of_week = {
-            0: "Monday",
-            1: "Tuesday",
-            2: "Wednesday",
-            3: "Thursday",
-            4: "Friday",
-            5: "Saturday",
-            6: "Sunday"
-        }
-
-        dates = [
-            'today',
-            'tomorrow',
-        ]
-
-        # show days of week up to next week
-        dow = datetime.date.today().weekday()
-        for i in range(dow + 2 % 7, dow + 7):
-            dates.append(days_of_week[i % 7])
-
-        # and some more relative days starting from next week
-        dates += ["1w", "2w", "1m", "2m", "3m", "1y"]
-
-        return dates
-
     to_absolute = lambda s: relative_date_to_date(s).isoformat()
 
     start_value_pos = p_word_before_cursor.find(':') + 1
     value = p_word_before_cursor[start_value_pos:]
 
-    for reldate in _date_suggestions():
+    for reldate in date_suggestions():
         if not reldate.startswith(value):
             continue
 
@@ -90,23 +48,24 @@ class TopydoCompleter(Completer):
     """
 
     def __init__(self, p_todolist):
-        self.todolist = p_todolist
+        self.inner_completer = CompleterBase(p_todolist)
 
     def _projects(self, p_word_before_cursor):
         """ Generator for project completion. """
-        projects = [p for p in self.todolist.projects() if
-                    p.startswith(p_word_before_cursor[1:])]
-
+        projects = self.inner_completer.projects(p_word_before_cursor)
         for project in projects:
-            yield Completion("+" + project, -len(p_word_before_cursor))
+            yield Completion(project, -len(p_word_before_cursor))
 
     def _contexts(self, p_word_before_cursor):
         """ Generator for context completion. """
-        contexts = [c for c in self.todolist.contexts() if
-                    c.startswith(p_word_before_cursor[1:])]
-
+        contexts = self.inner_completer.contexts(p_word_before_cursor)
         for context in contexts:
-            yield Completion("@" + context, -len(p_word_before_cursor))
+            yield Completion(context, -len(p_word_before_cursor))
+
+    def _subcmds(self, p_word_before_cursor):
+        subcommands = self.inner_completer.subcmds(p_word_before_cursor)
+        for command in subcommands:
+            yield Completion(command, -len(p_word_before_cursor))
 
     def get_completions(self, p_document, _):
         # include all characters except whitespaces (for + and @)
@@ -115,7 +74,7 @@ class TopydoCompleter(Completer):
                                      p_document.current_line_before_cursor)
 
         if is_first_word:
-            return _subcommands(word_before_cursor)
+            return self._subcmds(word_before_cursor)
         elif word_before_cursor.startswith('+'):
             return self._projects(word_before_cursor)
         elif word_before_cursor.startswith('@'):
