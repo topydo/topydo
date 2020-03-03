136645d02f04031f11bf50519b2141996095659c
David Steele <steele@debian.org>
2020-03-03 13:37:01 -0500

    Fix geo test time errors in tests
    The freezetime decorator was giving a time of the previous day if
    the test was run for time zones west of Greenwich.

--- test/test_list_format.py
+++ test/test_list_format.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import time
 import unittest
 from collections import namedtuple
 
@@ -35,7 +36,7 @@ except ImportError:
     import mock
 
 
-@freeze_time("2015, 11, 06")
+@freeze_time("2015-11-06 00:00:00", tz_offset = time.timezone/3600)
 class ListFormatTest(CommandTest):
     def setUp(self):
         super().setUp()
