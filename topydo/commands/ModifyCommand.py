203c52c2def5f2d7392d05c0a891c3a62a41b593
Dylan Leigh <dleigh@nuclear.dylanleigh.net>
2025-11-30 02:26:37 +1100

    Add "modify" command, "append" for multiple todos
    The modify command takes a single argument of text and appends it
    to multiple todo items, replacing start or due tags. For example:
    
         topydo modify '@PC' 34 36
         topydo modify t:tomorrow 13 23
    
    It is similar to the append command with arguments inverted
    (append takes one todo number and appends multiple text arguments)

---
+++ topydo/commands/ModifyCommand.py
@@ -0,0 +1,65 @@
+# Topydo - A todo.txt client written in Python.
+# Copyright (C) 2014 - 2015 Bram Schoenmakers <bram@topydo.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from topydo.lib.Command import InvalidCommandArgument
+from topydo.lib.Config import config
+from topydo.lib.prettyprinters.Numbers import PrettyPrinterNumbers
+from topydo.lib.TodoListBase import InvalidTodoException
+from topydo.lib.TodoParser import parse_line
+from topydo.lib.WriteCommand import WriteCommand
+
+
+class ModifyCommand(WriteCommand):
+    def __init__(self, p_args, p_todolist, #pragma: no branch
+                 p_out=lambda a: None,
+                 p_err=lambda a: None,
+                 p_prompt=lambda a: None):
+        super().__init__(p_args, p_todolist, p_out, p_err,
+                p_prompt)
+
+    def execute(self):
+        if not super().execute():
+            return False
+
+        try:
+            text = self.argument(0)
+            numbers = self.args[1:]
+
+            new_text_parsed = parse_line(text)
+            new_tags = new_text_parsed['tags']
+
+            for num in numbers:
+                todo = self.todolist.todo(num)
+                for tag in new_tags:
+                    if tag in (config().tag_start(), config().tag_due()):
+                        todo.remove_tag(tag)
+                self.todolist.append(todo, text)
+                self.postprocess_input_todo(todo)
+
+                self.printer.add_filter(PrettyPrinterNumbers(self.todolist))
+                self.out(self.printer.print_todo(todo))
+        except InvalidCommandArgument:
+            self.error(self.usage())
+        except InvalidTodoException:
+            self.error("Invalid todo number given.")
+
+    def usage(self):
+        return """Synopsis: modify <TEXT> <NUMBER> [<NUMBER 2> ...]"""
+
+    def help(self):
+        return """\
+Adds the TEXT to the end of the todo items indicated by NUMBER(s).\
+"""
